234813838ad21efc605de3a1e95c600b75904e92
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2025-08-18 07:16:10 +0500

    Add property to every object in array-7kyu

---
+++ 7kyu/AddPropertyToEveryObjectInArray/addPropertyToEveryObjectInArray.js
@@ -0,0 +1,16 @@
+//Ваша задача — добавить новое свойство usersAnswer к каждому объекту массива questions.
+// Значение usersAnswer должно быть равно NULL. Решение должно работать для массива любой длины.
+
+let questions = [{
+  question: "What's the currency of the USA?",
+  choices: ["US dollar", "Ruble", "Horses", "Gold"],
+  corAnswer: 0
+}, {
+  question: "Where was the American Declaration of Independence signed?",
+  choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+  corAnswer: 0
+}];
+
+for (let i = 0; i < questions.length; i++) {
+  questions[i].userAnswer = null;
+}

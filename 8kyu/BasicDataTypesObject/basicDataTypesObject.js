8c4ea3cb16567ef97c3b6828acb253a78fc6e160
Dmitry Bogdanov <dmtribogdanov@gmail.com>
2025-08-18 06:37:44 +0500

    Revert "Basic data types--Object-8kyu"
    This reverts commit 48344df67f61ba9fa6d4a2586cd8f95d038cccd3.

--- 8kyu/BasicDataTypesObject/basicDataTypesObject.js
+++
@@ -1,7 +0,0 @@
-//Напишите функцию animal, которая принимает параметр: obj и вернуть строку вроде этой: "This white dog has 4 legs."
-
-function animal(obj){
-  return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`;
-}
-
-console.log(animal({ name: "dog", legs: 4, color: "white" }))
